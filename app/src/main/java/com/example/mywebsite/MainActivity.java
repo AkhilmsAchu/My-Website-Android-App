33f47577eda21b9fbf86a2e823a0495c951100b5
Akhil MS <Akhilsree001@gmail.com>
2020-02-25 17:20:19 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mywebsite/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mywebsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv=(WebView) findViewById(R.id.webview);
+        wv.setInitialScale(1);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+        wv.loadUrl("http://akhilmsachu.me");
+    }
+}
