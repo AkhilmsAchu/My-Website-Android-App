a6102692d8e08d2edb0a458907014d3b4b53bc24
Akhil MS <Akhilsree001@gmail.com>
2020-02-27 07:35:42 +0000

    updates Task Description (name and icon)

--- app/src/main/java/com/example/mywebsite/MainActivity.java
+++ app/src/main/java/com/example/mywebsite/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.mywebsite;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -18,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
         wv.getSettings().setLoadWithOverviewMode(true);
         wv.getSettings().setUseWideViewPort(true);
         wv.loadUrl("http://akhilmsachu.me");
+        styleRecentTasksEntry(this);
     }
+    public static void styleRecentTasksEntry(Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+        Resources resources = activity.getResources();
+        String label = "My Website";
+        Bitmap icon = ((BitmapDrawable) activity.getApplicationInfo().loadIcon(activity.getPackageManager())).getBitmap();
+        activity.setTaskDescription(new ActivityManager.TaskDescription(label,icon));
+    }
+
 }
